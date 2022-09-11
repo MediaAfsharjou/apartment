8f8ac0ad3b462ae0471fd3b5a5de16e8d8015f2a
MediaAfsharjou <110778344+MediaAfsharjou@users.noreply.github.com>
2022-09-11 09:36:05 +0430

    Delete tenant-data.sql

--- apartment-management/tenant-data.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `empTable` (
-  `floor` int(4) NOT NULL auto_increment,
-  `unit` int(4) NOT NULL auto_increment,
-  `name` varchar(200) NOT NULL,
-  `info` varchar(200) NOT NULL,
-  PRIMARY KEY  (`floor`, `unit`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
